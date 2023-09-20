d59b900c66c29284b7a67451fa71e82c01920a40
Flacco-33 <lalo_hever@hotmail.com>
2023-09-19 19:58:22 -0600

    actividad

--- 2.1_Promesas/Promesas.js
+++ 2.1_Promesas/Promesas.js
@@ -1,52 +1,47 @@
 const btn = document.getElementById("btn");
 const div = document.getElementById("contenedor");
-let nombres=[];
+let nombres = [];
 
-async function Get(){
-    try {
-        const respuesta= await fetch("https://randomuser.me/api/");
-        const json = await respuesta.json();
-        alert(json.results[0].name.first)
-        // const nombre =json.results[0].name.first;
-        // alert(nombre);
-        // nombres.push(nombre);
-        // console.log(nombres);
-        
-        let card = document.createElement("div");
-        card.setAttribute("class","card");
-        card.setAttribute("style","width: 18rem");
-        div.appendChild(card);
-        let imagen = document.createElement("img")
-        imagen.setAttribute("src",json.results[0].picture.large)
-        imagen.setAttribute("class","card-img-top")
-        card.appendChild(imagen);
-        let body =document.createElement("div");
-        body.setAttribute("class","card-title")
-        body.setAttribute("value","flaco")
-        card.appendChild(body);
-        
+async function Get() {
+  try {
+    const respuesta = await fetch("https://randomuser.me/api/");
+    const json = await respuesta.json();
 
-     } catch (error) {
-         alert("Error Fatal, Comuniquese a soporte xD")
-        
-     }
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "card";
+    cardDiv.style.width = "18rem";
+
+    const img = document.createElement("img");
+    img.src = json.results[0].picture.large;
+    img.className = "card-img-top";
+
+    const cardBodyDiv = document.createElement("div");
+    cardBodyDiv.className = "card-body";
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    cardTitle.textContent = json.results[0].name.title +" "+json.results[0].name.first+" "+json.results[0].name.last;
+
+    const cardText = document.createElement("p");
+    cardText.className = "card-text";
+    cardText.textContent ="Ciudad: " + json.results[0].location.city +
+    "\nEstado: " + json.results[0].location.state +
+    "\nPais: " + json.results[0].location.country;
+
+    cardBodyDiv.appendChild(cardTitle);
+    cardBodyDiv.appendChild(cardText);
+    cardDiv.appendChild(img);
+    cardDiv.appendChild(cardBodyDiv);
+
+    div.appendChild(cardDiv);
+  } catch (error) {
+    alert("Error Fatal, Comuniquese a soporte xD");
+  }
 }
 
-{/* <div class="container">
-      <div class="card" style="width: 18rem">
-        <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div> */}
+{
+}
 
-btn.addEventListener("click",function(){
-    Get();
-    
+btn.addEventListener("click", function () {
+  Get();
 });
